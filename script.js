2649bc43c4a9869eea63a6899b158ac60b1c9f4a
gohjy <11069109-gohjy@users.noreply.replit.com>
2024-09-05 08:15:47 +0000

    Edit link to NLB page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (!footer) {
 	<hr/>
 	<h3><a href="./" target="main">gohjy Book Bugs (Main Page)</a></h3>
 	<p>
-		<a href="https://childrenandteens.nlb.gov.sg/events/bookbugsr/" target="official">Official Book Bugs Webpage</a>
+		<a href="https://www.nlb.gov.sg/main/site/discovereads/bookbugs-tales-of-travellers/Book-Bugs-Tales-of-Travellers" target="official">Official Book Bugs Webpage</a>
 	</p>
 	<p>
 		<a href="fortrade.html" target="fortrade">Cards for trade</a>
